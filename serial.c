5e639229054a60872bb88f2618fbd223b4ab691f
Laniakea <billiontrillionstars@gmail.com>
2024-09-09 13:20:25 -0400

    serial_putc (trial version)

---
+++ serial.c
@@ -0,0 +1,12 @@
+#include "serial.h"
+#include "utils.h"
+#define SERIAL_PORT 0x3f8
+#define SERIAL_OK (0x3fd>>5)&1
+
+void serial_init() {}
+
+void serial_putc(char ch)
+{
+    if SERIAL_OK
+        outb(SERIAL_PORT, ch);
+}
