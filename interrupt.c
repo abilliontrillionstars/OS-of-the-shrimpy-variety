5535a79dfcc8f1dcae8ce97819178cbec7792ded
Laniakea <112793967+abilliontrillionstars@users.noreply.github.com>
2024-10-03 21:30:08 -0400

    Fix Interrupts & turnin

--- interrupt.c
+++ interrupt.c
@@ -1,19 +1,32 @@
 #include "utils.h"
 #include "interrupt.h"
-//#include "asmstubs.c"
-
+#include "kprintf.h"
 #define NUM_INTERRUPTS 49
-static struct IDTEntry idt[NUM_INTERRUPTS];
+typedef void (*InterruptHandler)(struct InterruptContext* ctx);
+
 extern void* lowlevel_addresses[];
 //global variable
 static struct GDTEntry gdt[3]; 
+static struct IDTEntry idt[NUM_INTERRUPTS];
+
 
 void highlevel_handler(struct InterruptContext* ctx)
-{
-
+{ 
+    switch(ctx->interruptNumber)
+    {
+        case 0:
+            kprintf("Divide by zero"); break;
+        case 6:
+            kprintf("Undefined opcode"); break;
+        case 13:
+            kprintf("General fault"); break; // general fault! o7
+        default:
+            kprintf("waugh-errno-%d", ctx->errorcode); break;
+    }
+    while(1) asm("hlt");
 }
 
-asm(
+asm ( // this is a declaration of the midlevel handler in asm
     ".global _midlevel_handler\n"
     "_midlevel_handler:\n"
     "   push %ebp\n"
@@ -23,10 +36,10 @@ asm(
     "   push %ecx\n"
     "   push %ebx\n"
     "   push %eax\n"
-    "   push %esp\n" //address of stack top = addr of eax
+    "   push %esp\n"        //address of stack top = addr of eax
     "   cld\n"      //clear direction flag; C expects this
     "   call _highlevel_handler\n"
-    "   addl $4,%esp\n" //discard parameter for C
+    "   addl $4, %esp\n"    //discard parameter for C
     "   pop %eax\n"
     "   pop %ebx\n"
     "   pop %ecx\n"
@@ -45,10 +58,11 @@ void interrupt_init()
     lidt.size = sizeof(idt);
     lidt.addr = idt;
     asm volatile("lidt (%%eax)" : : "a"(&lidt));
+
     for (int i=0; i<NUM_INTERRUPTS; i++)
     {
         u32 a = (u32)(lowlevel_addresses[i]);
-        idt[i].addrLow = (u16)(a&0xff);
+        idt[i].addrLow = (u16)((a<<16)>>16);
         idt[i].addrHigh = (u16)(a>>16);
         idt[i].selector = 8;
         idt[i].zero = 0;
@@ -69,27 +83,34 @@ void gdt_init()
     gdt[1].baseBits16to23 = 0;
     gdt[1].baseBits24to31 = 0;
     gdt[1].limitBits0to15 = 0xffff;
-    gdt[1].flags = SEGMENT_TYPE_CODE | SEGMENT_IS_CODE_OR_DATA | 
-                    SEGMENT_RING_0 | SEGMENT_PRESENT;
-    gdt[1].flagsAndLimitBits16to19 = SEGMENT_LIMIT_HIGH_NIBBLE |
-                    SEGMENT_LIMIT_32_BIT | SEGMENT_32_BIT;
+    gdt[1].flags = SEGMENT_TYPE_CODE |
+                   SEGMENT_IS_CODE_OR_DATA |
+                   SEGMENT_RING_0 |
+                   SEGMENT_PRESENT ;
+    gdt[1].flagsAndLimitBits16to19 =
+                    SEGMENT_LIMIT_HIGH_NIBBLE |
+                    SEGMENT_LIMIT_32_BIT |
+                    SEGMENT_32_BIT;
     
-    gdt[2].baseBits0to15 = 0;
-    gdt[2].baseBits16to23 = 0;
-    gdt[2].baseBits24to31 = 0;
-    gdt[2].limitBits0to15 = 0xffff;
-    gdt[2].flags = SEGMENT_TYPE_CODE | SEGMENT_IS_CODE_OR_DATA | 
-                    SEGMENT_RING_0 | SEGMENT_PRESENT;
-    gdt[2].flagsAndLimitBits16to19 = SEGMENT_LIMIT_HIGH_NIBBLE |
-                    SEGMENT_LIMIT_32_BIT | SEGMENT_32_BIT;
+    gdt[2].baseBits0to15=0;
+    gdt[2].baseBits16to23=0;
+    gdt[2].baseBits24to31=0;
+    gdt[2].limitBits0to15=0xffff;
+    gdt[2].flags = SEGMENT_TYPE_DATA |
+                   SEGMENT_IS_CODE_OR_DATA |
+                   SEGMENT_RING_0 |
+                   SEGMENT_PRESENT ;
+    gdt[2].flagsAndLimitBits16to19 =
+                    SEGMENT_LIMIT_HIGH_NIBBLE |
+                    SEGMENT_LIMIT_32_BIT |
+                    SEGMENT_32_BIT;
     
 
     struct LGDT lgdt; // alphabet mafia has infiltrated your machine
     lgdt.size = sizeof(gdt);
     lgdt.addr = &gdt[0];
     void* tmp = &lgdt;
-    
-    asm volatile(
+    asm volatile (
          "lgdt (%%eax)\n"       //load gdt register
          "mov $16,%%eax\n"      //set eax to 16: Offset to gdt[2]
          "mov %%eax,%%ds\n"     //store 16 to ds
@@ -101,7 +122,7 @@ void gdt_init()
          "reset_cs:\n"
          "nop\n"                //no operation
          : "+a"(tmp)
-         : : "memory"
+         :
+         : "memory"
     );
-
 }
--- kmain.c
+++ kmain.c
@@ -14,23 +14,27 @@ __asm__(
     "call _kmain"
 );
 
+void sweet();
+
 static struct MultibootInfo bootInfo;
 
 void kmain(struct MultibootInfo* mbi)
 {
-    //set up the things
+    // set up the things
     kmemcpy(&bootInfo, mbi, sizeof(bootInfo));
     console_init(mbi);
     memory_init(mbi);
     serial_init();
 
-    gdt_init(); // move this back to inside interrupt_init if things get hairy
-    //interrupt_init();
+    gdt_init();
+    interrupt_init();
 
     // now do the things
-    kprintf("Everyone's programmed differently.\n");
-    kprintf("this will be overwritten! @\x7f \rthat");
+    //kprintf("Everyone's programmed differently.\n");
+    //kprintf("this will be overwritten! @\x7f \rthat");
     
+    sweet();
+
     const char* string = "\nDONE\n";
     for(int i=0; string[i]; i++)
         serial_putc(string[i]);
