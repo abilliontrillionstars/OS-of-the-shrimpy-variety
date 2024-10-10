f7362d8729654c8cfc2559ffc03dd57a8c4906f9
Laniakea <billiontrillionstars@gmail.com>
2024-10-10 17:27:42 -0400

    integrate malloc sln 1/2

--- memory.c
+++ memory.c
@@ -21,90 +21,96 @@ void memory_init(struct MultibootInfo* info)
         kprintf("Region %d: addr=0x%08x...0x%08x length%dKB type=%s\n",
             i, M[i].addr, end-1, M[i].length/1024, (M[i].type==1) ? "RAM":"Reserved");
     }
+
+    for(unsigned i=0; i<HEAP_ORDER; ++i)
+    {
+        freeList[i] = NULL;
+        freeList[HEAP_ORDER] = (Header*)heap;
+        initHeader(freeList[HEAP_ORDER], HEAP_ORDER);
+    }
 }
+
 
 void* kmalloc(u32 size)
 {
-    size = size+sizeof(Header*);
-    unsigned o = 6; //min is 64KB
+    size = size + sizeof(Header);
+    if(size > (1<<HEAP_ORDER))
+        return NULL;
 
-    //bring order up to the power of 2 that we need for the given size
-    while((1<<o) < size) o++; 
-    int i = o;
-    //scan through freeList (which is an array of Header LINKED LISTS) 
-    //until we find the right order
-    while(i <= HEAP_ORDER && !freeList[i]) i++;
-    if(i > HEAP_ORDER) return NULL;
+    //minimum size = 64 bytes = 2**6
+    unsigned order = 6;
+    while((1<<order) < size) order++;
+    unsigned i = order;
+    while(i<=HEAP_ORDER && freeList[i]==NULL) i++;
 
-    while(i>o)
+    if(i > HEAP_ORDER)
+        return NULL; //not enough memory
+
+    while(i>order)
     {
-        //this is where we split our memory blocks
-        bisect(o, i);
+        splitBlock(i);
         i--;
     }
-    //now we have an entry in freeList of our desired order,
 
-    //TODO <--
-    Header* h = removeFromFreeList(i);
-    h->used = 1;
-    return ((char*)h + sizeof(Header));
+    Header* h = freeList[i];
+    removeFirstNode(i);
+    h->used=1;
+    char* c = (char*) h;
+    return c + sizeof(Header);
 }
 
-void kfree(void* ptr)
+void kfree(void* v)
 {
-    Header* h = (Header*)ptr;
-    // IMPORTANT
+    char* c = (char*) v;
+    Header* h = (Header*)(c-sizeof(Header));
     h->used = 0;
-
-
-
-    addToFreeList(h);
+    prependNode(h);
+    while(1)
+    {
+        if(h->order==HEAP_ORDER)
+            return;     //entire heap is free
+        Header* b = getBuddy(h);
+        if(b->used == 0 && b->order == h->order)
+        {
+            removeNode(h);
+            removeNode(b);
+            if(b<h) h=b;
+            h->order++;
+            prependNode(h);
+        }
+        else
+            return;
+    }
 }
-
 // header-helper functions (why is it always hamburger help her why is it never hamburger help me)
 static void initHeader(Header* h, unsigned order)
 {
     h->used = 0;
     h->order = order;
-    h->prev = (u32) h;
-    h->next = (u32) h; 
+    setPrev(h,NULL);
+    setNext(h,NULL);
 }
 Header* getBuddy(Header* h)
 {
-    u32 delta = ((char*)h - heap);
-    //flip the bit at position h->order
-    delta = delta ^ 1<<h->order;
-
-    return (Header*)(delta+heap);
+    char* c = (char*) h;
+    unsigned offset = c-heap;
+    offset ^= (1<<h->order);
+    c = heap + offset;
+    return (Header*)c;
 }
 
-
-
-
-
-void bisect(unsigned order, unsigned index) 
+void bisect(unsigned index) 
 {
-    // take block off of freelist[i]
-
-    // split it in half
-    
-    // add two to freelist[i-1]
-    
-    Header* h = removeFromFreeList(index);
-    h->order--;
-    Header * h2 = (Header*)((char*)h + (1<<(h->order)));
-    initHeader(h2, h->order);
+    Header* h = freeList[index];
+    removeFirstNode(index);
+    h->order = h->order-1;
+    char* c = (char*) h;
+    c += (1<<h->order);
+    Header* h2 = (Header*) c;
+    initHeader(h2,h->order);
+    prependNode(h);
+    prependNode(h2);
 }
-void coalesce(Header* h) 
-{
-    Header* b = getBuddy(h);
-    // do linked list stuff
-
-
-    addToFreeList(h);
-    addToFreeList(b);
-}
-
 
 void addToFreeList(Header* h) 
 {
@@ -115,3 +121,70 @@ Header* removeFromFreeList(unsigned order)
     return (Header*) NULL;
 }
 
+static Header* getNext(Header* h)
+{
+    unsigned delta = (h->next) << 6;
+    Header* h2 = (Header*)(heap+delta);
+    if( h == h2 )
+        return NULL;
+    return h2;
+}
+static void setNext(Header* h, Header* next)
+{
+    if(next== NULL)
+        next = h;
+    char* c = (char*) next;
+    unsigned delta = c-heap;
+    delta >>= 6;
+    h->next = delta;
+}
+static Header* getPrev(Header* h)
+{
+    unsigned delta = (h->prev) << 6;
+    Header* h2 = (Header*)(heap+delta);
+    if( h == h2 )
+        return NULL;
+    return h2;
+}
+static void setPrev(Header* h, Header* prev)
+{
+    if( prev == NULL )
+        prev = h;
+    char* c = (char*) prev;
+    unsigned delta = c-heap;
+    delta >>= 6;
+    h->prev = delta;
+}
+
+void removeFirstNode(unsigned i)
+{
+    Header* h = freeList[i];
+    Header* n = getNext(h);
+    if(n)
+        setPrev(n,NULL);
+    freeList[i] = n;
+}
+void removeNode(Header* h)
+{
+    Header* p = getPrev(h);
+    Header* n = getNext(h);
+    if(p)
+        setNext(p,n);
+    if(n)
+        setPrev(n,p);
+
+    //if we're removing the first node,
+    //need to update head
+    if(freeList[h->order] == h)
+        freeList[h->order] = n;
+}
+void prependNode(Header* h)
+{
+    unsigned i = h->order;
+    Header* n = freeList[i];
+    setPrev(h, NULL);
+    setNext(h, n);
+    if(n)
+        setPrev(n,h);
+    freeList[i] = h;
+}
--- memory.h
+++ memory.h
@@ -11,9 +11,18 @@ void kfree(void* ptr);
 
 //many a helper function!
 Header* getBuddy(Header* h);
-void bisect(unsigned order, unsigned index);
-void coalesce(Header* h);
+void bisect(unsigned order);
+
 // that is, "sell" a chunk *back* to the freeList market
 void addToFreeList(Header* h);
 // that is, "buy" a chunk *from* the available real estate
 Header* removeFromFreeList(unsigned order);
+
+static Header* getNext(Header* h);
+static void setNext(Header* h, Header* next);
+static Header* getPrev(Header* h);
+static void setPrev(Header* h, Header* prev);
+
+void removeFirstNode(unsigned i);
+void removeNode(Header* h);
+void prependNode(Header* h);
